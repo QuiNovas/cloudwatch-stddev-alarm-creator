f078761bcc56a5dbe61063d65fd429c009f20921
Joseph Wortmann <joseph.wortmann@gmail.com>
2019-05-30 10:21:31 -0500

    Updated README
    And some env var names

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 
-lambda_name = 'cloudwatch-stdev-alert-calculator'
+lambda_name = 'cloudwatch-stdev-alarm-creator'
 
 # Versions should comply with PEP440.  For a discussion on single-sourcing
 # the version across setup.py and the project code, see
--- src/lambda_function/function.py
+++ src/lambda_function/function.py
@@ -9,20 +9,20 @@ from statistics import mean, pstdev
 
 
 ALARM_ACTIONS = [ x.strip() for x in environ.get('ALARM_ACTIONS', '').split(',') if x ]
-ALARM_BOUNDS = environ.get('ALARM_BOUNDS', 'Both')
-ALARM_DATAPOINTS_TO_ALARM = int(environ['ALARM_DATAPOINTS_TO_ALARM'])
-ALARM_EVALUATION_PERIODS = int(environ['ALARM_EVALUATION_PERIODS'])
-ALARM_PERIOD = int(environ['ALARM_PERIOD'])
+BOUNDS = environ.get('BOUNDS', 'Both')
 CLOUDWATCH = boto3.client('cloudwatch')
+DATAPOINTS_TO_ALARM = int(environ['DATAPOINTS_TO_ALARM'])
+EVALUATION_PERIODS = int(environ['EVALUATION_PERIODS'])
 INSUFFICIENT_DATA_ACTIONS = [ x.strip() for x in environ.get('INSUFFICIENT_DATA_ACTIONS', '').split(',') if x ]
-METRIC_NAMESPACE = environ['METRIC_NAMESPACE']
-METRIC_NAME = environ['METRIC_NAME']
 METRIC_DIMENSIONS = {key: value for (key, value) in [ [ y.strip() for y in x.strip().split(',') if y ] for x in environ.get('METRIC_DIMENSIONS','').split(';') if x ]}
+METRIC_NAME = environ['METRIC_NAME']
+METRIC_NAMESPACE = environ['METRIC_NAMESPACE']
 METRIC_SAMPLE_DAYS = int(environ.get('METRIC_SAMPLE_DAYS', '15'))
 METRIC_STAT = environ['METRIC_STAT']
 METRIC_UNIT = environ['METRIC_UNIT']
 NUM_STANDARD_DEVIATION = int(environ.get('NUM_STANDARD_DEVIATION', '3'))
 OK_ACTIONS = [ x.strip() for x in environ.get('OK_ACTIONS', '').split(',') if x ]
+PERIOD = int(environ['PERIOD'])
 
 logger = logging.getLogger()
 logger.setLevel(logging.INFO)
@@ -40,23 +40,23 @@ def handler(event, context):
       high_threshold = data_mean + (data_pstdev * NUM_STANDARD_DEVIATION)
       low_threshold = data_mean - (data_pstdev * NUM_STANDARD_DEVIATION)
       logger.info('Mean: {}; Stddev: {}; High: {}; Low: {}'.format(data_mean, data_pstdev, high_threshold, low_threshold))
-      if ALARM_BOUNDS == 'Both':
+      if BOUNDS == 'Both':
         _put_metric_alarm(metric, period, 'AlarmHigh', high_threshold)
         if low_threshold > 0.0:
           _put_metric_alarm(metric, period, 'AlarmLow', low_threshold)
         else:
           _delete_metric_alarm(metric, 'AlarmLow')
-      elif ALARM_BOUNDS == 'AlarmHigh':
+      elif BOUNDS == 'AlarmHigh':
         _put_metric_alarm(metric, period, 'AlarmHigh', high_threshold)
         _delete_metric_alarm(metric, 'AlarmLow')
-      elif ALARM_BOUNDS == 'AlarmLow':
+      elif BOUNDS == 'AlarmLow':
         if low_threshold > 0.0:
           _put_metric_alarm(metric, period, 'AlarmLow', low_threshold)
         else:
           _delete_metric_alarm(metric, 'AlarmLow')
         _delete_metric_alarm(metric, 'AlarmHigh')
       else:
-        raise ValueError('ALARM_BOUNDS {} unrecognized, must be one of AlarmHigh, AlarmLow, or Both'.format(ALARM_BOUNDS))
+        raise ValueError('ALARM_BOUNDS {} unrecognized, must be one of AlarmHigh, AlarmLow, or Both'.format(BOUNDS))
   return event
 
 
@@ -101,8 +101,8 @@ def _put_metric_alarm(metric, period, bound, threshhold):
     Dimensions=metric['Dimensions'],
     Period=period,
     Unit=METRIC_UNIT,
-    EvaluationPeriods=ALARM_EVALUATION_PERIODS,
-    DatapointsToAlarm=ALARM_DATAPOINTS_TO_ALARM,
+    EvaluationPeriods=EVALUATION_PERIODS,
+    DatapointsToAlarm=DATAPOINTS_TO_ALARM,
     Threshold=threshhold,
     ComparisonOperator='GreaterThanThreshold' if bound == 'AlarmHigh' else 'LessThanThreshold'
   )
@@ -111,15 +111,15 @@ def _put_metric_alarm(metric, period, bound, threshhold):
 def _calculate_period():
   end_time = datetime.utcnow().replace(second=0,microsecond=0)
   if METRIC_SAMPLE_DAYS <= 15:
-    period = max(ALARM_PERIOD, 60)
+    period = max(PERIOD, 60)
   elif METRIC_SAMPLE_DAYS <= 63:
-    period = max(ALARM_PERIOD, 300)
+    period = max(PERIOD, 300)
     end_time = end_time.replace(minute=end_time.minute-end_time.minute%5)
   elif METRIC_SAMPLE_DAYS <= 455:
-    period = max(ALARM_PERIOD, 3600)
+    period = max(PERIOD, 3600)
     end_time.replace(minute=0)
   else:
-    raise ValueError('SAMPLE_PERIOD_DAYS cannot be greater than 455')
+    raise ValueError('METRIC_SAMPLE_DAYS cannot be greater than 455')
   return end_time - timedelta(days=METRIC_SAMPLE_DAYS), end_time, period
 
 
@@ -137,6 +137,7 @@ def _do_dimensions_match(dimensions):
     if not re.match(METRIC_DIMENSIONS[dimension['Name']], dimension['Value']):
       return False
   return True
+
 
 def _list_metrics(next_token=None):
   if next_token:
